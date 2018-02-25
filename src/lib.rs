39668582f891678bacf5364d14c21bb463c28871
Stephen Sherratt <sfsherratt@gmail.com>
2018-02-25 17:47:15 +0000

    Allow possibly different storage to appending type

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,43 @@
 use std::collections::VecDeque;
 
 pub trait Append<T> {
+    type Common: From<T>;
     fn append(&mut self, value: T);
 }
 
-impl<T> Append<T> for Vec<T> {
+impl<T, S: From<T>> Append<T> for Vec<S> {
+    type Common = S;
     fn append(&mut self, value: T) {
-        self.push(value);
+        self.push(value.into());
     }
 }
 
-impl<T> Append<T> for VecDeque<T> {
+impl<T, S: From<T>> Append<T> for VecDeque<S> {
+    type Common = S;
     fn append(&mut self, value: T) {
-        self.push_back(value);
+        self.push_back(value.into());
     }
+}
+
+#[test]
+fn foo() {
+    enum Foo {
+        Bar,
+        Baz(u64),
+    }
+
+    impl From<u64> for Foo {
+        fn from(u: u64) -> Self {
+            Foo::Baz(u)
+        }
+    }
+
+    fn f<A: Append<Foo> + Append<u64>>(a: &mut A) {
+        a.append(Foo::Bar);
+        a.append(0);
+    }
+
+    let mut v: Vec<Foo> = Vec::new();
+
+    f(&mut v);
 }
